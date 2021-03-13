07ca6bb5da35712b06560b4347cdfdc05cd53a03
Vishal Jotwani <vishaljotwani48@gmail.com>
2021-03-13 20:55:01 +0530

    implementing logic

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
+//function to compute interest
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var p = document.getElementById("principal").value;
+    if (p == "" || p <= 0)
+    {
+	alert("Enter a positive number");
+	p.focus();
+	return;
+    }
+    var r = document.getElementById("rate").value;
+    var y = document.getElementById("years").value;
+
+    var i = (p * r * y) / 100;
+
+    var d = new Date().getFullYear();
+    var cy = parseInt(y) + parseInt(d);
+
+    document.getElementById("result").innerHTML = "If you deposit <mark> " + p +
+                                                    "</mark>,<br> at an interest rate of <mark> " +
+                                                    r + "%</mark>. <br> You will receive an amount of <mark>" +
+                                                    i + "</mark>,<br> in the year <mark>" + cy;
     
 }
-        
+
+//function to display rate as slider change
+function displayRate(){
+    var r = document.getElementById("rate").value;
+    document.getElementById("selectedRate").innerHTML = r;
+}
